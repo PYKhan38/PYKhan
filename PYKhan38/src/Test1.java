2b59bdbbea83a728eb54d2f651199cc5970a78fe
PYKhan38 <patanyousafkhan38@gmail.com>
2022-12-19 11:29:47 +0530

    This is my first commit.........

---
+++ PYKhan38/src/Test1.java
@@ -0,0 +1,21 @@
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		
+		
+		String hungry= "eat";
+		
+		if(hungry== "eat") {
+			
+			System.out.println("Eat food.........");
+		}
+		
+		else {
+			
+			System.out.println("Do work.........");			
+		}			
+
+	}
+
+}
